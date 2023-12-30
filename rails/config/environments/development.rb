2bb7a415d132a4f51839675ec025d30310249d73
tomoprogra <anyonewww@outlook.jp>
2023-12-30 23:19:01 +0900

    修正

--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -17,6 +17,10 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+  # 認証メール送信に関する設定
+  config.action_mailer.default_options = { from: "no-replay@example.com" }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.delivery_method = :letter_opener_web
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
